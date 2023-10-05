4d8bde4c64ac04830d5d4fb89cd2913c9dc9f54f
JessicaS161995 <ojitos16@outlook.es>
2023-10-05 16:52:42 -0500

    arreglos del profe

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let miPelota;
+let frutas = ["manzana", miPelota, 234, "granadilla"];
+
+for (let LQS = 0; LQS < 10; LQS += 1){
+  console.log(LQS)
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  miPelota = new Pelota();
 }
 
 function draw() {
+  background(255);
   circle(mouseX, mouseY, 20);
+  miPelota.update(300);
+  miPelota.display();
 }
